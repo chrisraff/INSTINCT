92e42b5f538a611586f8bc0f7618cbe5cb2202d3
AlexLamson <alex.lamson@gmail.com>
2019-04-06 13:05:21 -0400

    added in my idea to prevent track self-collision

--- track_generation.py
+++ track_generation.py
@@ -17,10 +17,11 @@ def createCircle():
 
     return points
 
+
 def plotPoints(points):
     plt.figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
-    plt.xlim(-1.5, 1.5)
-    plt.ylim(-1.5, 1.5)
+    plt.xlim(-2.0, 2.0)
+    plt.ylim(-2.0, 2.0)
 
     for i in range(len(points)):
         if i < len(points)-1:
@@ -32,24 +33,33 @@ def plotPoints(points):
     # plt.show()
     return
 
+
 def alterPoints(points):
+    newpoints = np.copy(points)
 
-    # should
+    # push the points in and out away from the center of the circle
+    max_expand_distance = 0.5
+    max_contract_distance = 0.2
+
     theta = np.arctan2(points[:,1], points[:,0])
+    delta_distance = np.random.uniform(-max_contract_distance, max_expand_distance, len(points))
+    noise = np.array([delta_distance * np.cos(theta), delta_distance * np.sin(theta)]).T
+    newpoints += noise
 
-    noise = np.random.uniform(-0.5, 0.5, len(points)*2).reshape(points.shape)
-    newpoints = points + noise
-    newpoints[-1,:] = newpoints[0,:]
+    # on top of the previous transformation, add a random delta
+    max_uniform_delta = 0.5
+
+    noise = np.random.uniform(-max_uniform_delta, max_uniform_delta, len(points)*2).reshape(points.shape)
+    newpoints = newpoints + noise
+    newpoints[-1,:] = newpoints[0,:]  # not sure why this line is needed
+
     # print(points)
     # print(points + noise)
 
     return newpoints
 
 
-
-
 def main():
-
     points = createCircle()
     # plotPoints(points)
     newpoints = alterPoints(points)
@@ -58,13 +68,11 @@ def main():
     num_output_points = 1000
 
     # https://stackoverflow.com/questions/33962717/interpolating-a-closed-curve-using-scipy
-    tck, _ = interpolate.splprep([newpoints[:,0], newpoints[:,1]], s=0, per=True)
+    tck, _ = interpolate.splprep(newpoints.T, s=0, per=True)
     out = interpolate.splev(np.linspace(0, 1, num_output_points), tck)
-
 
     plt.plot(out[0], out[1])
     plt.show()
-
 
 
 if __name__ == "__main__":
