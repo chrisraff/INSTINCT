ab061c0ebbd2ef9b0222868e3eddd27191ef1b00
AlexLamson <alex.lamson@gmail.com>
2019-04-06 14:58:32 -0400

    added sides of tracks

--- track_generation.py
+++ track_generation.py
@@ -4,6 +4,7 @@ import numpy as np
 from math import *
 from scipy.interpolate import interp1d
 from scipy import interpolate
+from numpy.linalg import norm
 
 
 def createCircle():
@@ -69,34 +70,70 @@ def alterPoints(points):
 
 
 def make_track():
+    num_output_points = 1000
+    track_width = 0.05
+
+
     points = createCircle()
     # plotPoints(points)
     control_points = alterPoints(points)
     # plotPoints(control_points)
-
-    num_output_points = 1000
 
     # https://stackoverflow.com/questions/33962717/interpolating-a-closed-curve-using-scipy
     tck, _ = interpolate.splprep(control_points.T, s=0, per=True)
     track_points = interpolate.splev(np.linspace(0, 1, num_output_points), tck)
     track_points = np.array(track_points).T
 
-    return track_points, control_points
+    # get the left and right sides of the track
+    left_track = []
+    right_track = []
+
+    for i in range(len(track_points)):
+        p0 = track_points[i-1]
+        p1 = track_points[i]
+        v = p1-p0  # forward
+
+        delta_left = np.array([-v[1], v[0]])
+        delta_left = track_width * delta_left / norm(delta_left)
+
+        delta_right = np.array([v[1], -v[0]])
+        delta_right = track_width * delta_right / norm(delta_right)
+
+        left_track += [delta_left]
+        right_track += [delta_right]
+
+    left_track = np.array(left_track) + track_points
+    right_track = np.array(right_track) + track_points
+
+
+    left_track[0] = left_track[-1]
+    right_track[0] = right_track[-1]
+
+    return track_points, control_points, left_track, right_track
 
 
 def main():
-    # track_points, control_points = make_track()
-    # plt.plot(track_points[0], track_points[1])
+    # # plot one track for debugging
+    # track_points, control_points, left_track, right_track = make_track()
+    # plt.plot(left_track[:,0], left_track[:,1])
+    # plt.plot(right_track[:,0], right_track[:,1])
+    # # plt.plot(track_points[:,0], track_points[:,1])
+    # plt.axis('equal')
     # plt.show()
 
     # plot lots of tracks to get a better idea of the results
-    f, axes = plt.subplots(4, 8)
+    f, axes = plt.subplots(3, 5)8
+    # f, axes = plt.subplots(4, 8)
     f.subplots_adjust(left=0,right=1,bottom=0,top=1)
     for ax_row in axes:
         for ax in ax_row:
-            track_points, control_points = make_track()
+            track_points, control_points, left_track, right_track = make_track()
             ax.scatter(control_points[:,0], control_points[:,1], c='r')
-            ax.plot(track_points[:,0], track_points[:,1], c='b')
+
+            ax.plot(left_track[:,0], left_track[:,1], c='g')
+            ax.plot(right_track[:,0], right_track[:,1], c='g')
+            # ax.plot(track_points[:,0], track_points[:,1], c='b')
+
             ax.axis('equal')  # preserve aspect ratio
             ax.axis('off')
     plt.show()
