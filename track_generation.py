f73c28dc38257dbc5fefb073f4d76fe76979355c
AlexLamson <alex.lamson@gmail.com>
2019-04-06 13:49:54 -0400

    made tracks have more turns

--- track_generation.py
+++ track_generation.py
@@ -35,43 +35,58 @@ def plotPoints(points):
 
 
 def alterPoints(points):
-    newpoints = np.copy(points)
+    control_points = np.copy(points)
 
     # push the points in and out away from the center of the circle
+    min_expand_distance = 0.2
     max_expand_distance = 0.5
     max_contract_distance = 0.2
 
     theta = np.arctan2(points[:,1], points[:,0])
-    delta_distance = np.random.uniform(-max_contract_distance, max_expand_distance, len(points))
+    # delta_distance = np.random.uniform(-max_contract_distance, max_expand_distance, len(points))
+
+    # alternate pushing and pulling to increase number of turns
+    delta_distance = np.empty((len(points),))
+    delta_distance[::2] = 1
+    delta_distance[1::2] = -1
+    delta_distance *= np.random.uniform(min_expand_distance, max_expand_distance, len(points))
+
     noise = np.array([delta_distance * np.cos(theta), delta_distance * np.sin(theta)]).T
-    newpoints += noise
+    control_points += noise
+
 
     # on top of the previous transformation, add a random delta
-    max_uniform_delta = 0.5
+    max_uniform_delta = 0.4
 
     noise = np.random.uniform(-max_uniform_delta, max_uniform_delta, len(points)*2).reshape(points.shape)
-    newpoints = newpoints + noise
-    newpoints[-1,:] = newpoints[0,:]  # not sure why this line is needed
+    control_points = control_points + noise
+    control_points[-1,:] = control_points[0,:]  # not sure why this line is needed
 
     # print(points)
     # print(points + noise)
 
-    return newpoints
+    return control_points
 
 
-def main():
+def make_track():
     points = createCircle()
     # plotPoints(points)
-    newpoints = alterPoints(points)
-    plotPoints(newpoints)
+    control_points = alterPoints(points)
+    # plotPoints(control_points)
 
     num_output_points = 1000
 
     # https://stackoverflow.com/questions/33962717/interpolating-a-closed-curve-using-scipy
-    tck, _ = interpolate.splprep(newpoints.T, s=0, per=True)
-    out = interpolate.splev(np.linspace(0, 1, num_output_points), tck)
+    tck, _ = interpolate.splprep(control_points.T, s=0, per=True)
+    track_points = interpolate.splev(np.linspace(0, 1, num_output_points), tck)
+    track_points = np.array(track_points).T
 
-    plt.plot(out[0], out[1])
+    return track_points, control_points
+
+
+def main():
+    track_points, control_points = make_track()
+    plt.plot(track_points[0], track_points[1])
     plt.show()
 
 
