4b3f72d6808139a7333ad6fb6f6c7e50f207fa5a
AlexLamson <alex.lamson@gmail.com>
2019-04-08 20:59:35 -0400

    random deltas are uniform circles instead of squares

--- track_generation.py
+++ track_generation.py
@@ -37,31 +37,35 @@ def plotPoints(points):
 
 def alterPoints(points):
     control_points = np.copy(points)
+    theta = np.arctan2(points[:,1], points[:,0])
+
+    max_uniform_delta_radius = 0.4
+    max_contract_distance = -0.2
+    max_expand_distance = 0.5
 
     # push the points in and out away from the center of the circle
-    min_expand_distance = 0.2
-    max_expand_distance = 0.5
-    max_contract_distance = 0.2
-
-    theta = np.arctan2(points[:,1], points[:,0])
     # delta_distance = np.random.uniform(-max_contract_distance, max_expand_distance, len(points))
+    # noise = np.array([delta_distance * np.cos(theta), delta_distance * np.sin(theta)]).T
+    # control_points += noise
 
     # alternate pushing and pulling to increase number of turns
     delta_distance = np.empty((len(points),))
     delta_distance[::2] = 1
     delta_distance[1::2] = -1
-    delta_distance *= np.random.uniform(min_expand_distance, max_expand_distance, len(points))
-
+    delta_distance *= np.random.uniform(max_contract_distance, max_expand_distance, len(points))
     noise = np.array([delta_distance * np.cos(theta), delta_distance * np.sin(theta)]).T
     control_points += noise
 
+    # add a random delta to each control point, sampled uniformly over a circle
+    # https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly
+    dr = max_uniform_delta_radius * np.sqrt(np.random.uniform(size=len(points)))
+    dt = np.random.uniform(size=len(points)) * 2 * pi
+    control_points[:,0] += dr*np.cos(dt)
+    control_points[:,1] += dr*np.sin(dt)
 
-    # on top of the previous transformation, add a random delta
-    max_uniform_delta = 0.4
 
-    noise = np.random.uniform(-max_uniform_delta, max_uniform_delta, len(points)*2).reshape(points.shape)
-    control_points = control_points + noise
-    control_points[-1,:] = control_points[0,:]  # not sure why this line is needed
+
+    # control_points[-1,:] = control_points[0,:]  # not sure why this line is needed
 
     # print(points)
     # print(points + noise)
